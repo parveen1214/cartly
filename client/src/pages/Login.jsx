6a9c6f6c28fc9c7cc22892c55816bfe8e9b18d41
parveen1214 <parveen1214k@gmail.com>
2026-02-27 00:32:04 +0530

    deleting the env file

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+   
   const handleSubmit = async (e) => {
     e.preventDefault();
 
